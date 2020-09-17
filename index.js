f00bd5b41e28461e3e99f906601e54531d630762
Norman Oliden <naoliden@uc.cl>
2020-09-17 18:09:23 -0300

    routes done

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const cors = require('cors');
 // const http = require('http');
 // const morgan = require('morgan');
+const PORT = 5000;
+const HOSTNAME = 'localhost';
 
 
 const app = express();
-const PORT = 5000;
-const HOSTNAME = 'localhost'
 
 // Middleware
 app.use(express.json()); //req.body
@@ -14,15 +14,13 @@ app.use(cors());
 // app.use(morgan('dev'));
 
 
-// Routes
-
-
-
+// ROUTES
 // register and login routes
+app.use("/auth", require('./routes/jwtAuth'));
 
-app.use("/auth", require('./routes/jwtAuth'))
-
+// dashboard
+app.use("/dashboard", require('./routes/dashboard'));
 
 app.listen(PORT, HOSTNAME, () => {
   console.log(`Server is running on port ${PORT}`)
-})
+});
